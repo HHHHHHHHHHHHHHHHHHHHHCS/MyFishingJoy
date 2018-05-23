a6c32d11f1690a61717ba5b0bfa514e51d7653e1
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-05-23 11:45:06 +0800

    AB 包和LUA 脚本网络化

--- Assets/My/Fish/Scripts/MyFishLua01.cs
+++ Assets/My/Fish/Scripts/MyFishLua01.cs
@@ -1,20 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Net;
 using UnityEngine;
+using UnityEngine.Networking;
 using XLua;
 
 public class MyFishLua01 : MonoBehaviour
 {
     private const string fileDir = "My/Fish/Lua";
     private const string filePath = "MyFishLua01.lua.txt";
+    private const string webPath = @"http://127.0.0.1/MyFishingJoy/";
 
     private LuaEnv luaEnv;
 
+    //本地的
+    /*
     private static Dictionary<string, GameObject> gameObjectDic;
     private static Queue<AssetBundle> AssetBundleQueue;
+    */
+    private Dictionary<string, GameObject> gameObjectDic;
+    private Queue<AssetBundle> AssetBundleQueue;
 
-    private void Awake()
+    //本地的
+    /* private void Awake()
     {
         gameObjectDic = new Dictionary<string, GameObject>();
         AssetBundleQueue = new Queue<AssetBundle>();
@@ -23,17 +32,36 @@ public class MyFishLua01 : MonoBehaviour
         luaEnv.AddLoader(MyLoader);
         luaEnv.DoString("require 'MyFishLua01';");
         luaEnv.DoString("HotFix(true);");
+    }*/
+
+
+    private void Awake()
+    {
+        gameObjectDic = new Dictionary<string, GameObject>();
+        AssetBundleQueue = new Queue<AssetBundle>();
+        luaEnv = new LuaEnv();
+        luaEnv.DoString("require 'xlua.util';");
+        LoadLua();
 
     }
 
-    private void Update()
+    //网络
+    private void Start()
     {
-        while(AssetBundleQueue.Count>0)
+
+    }
+
+    //本地
+    /*
+    private void LateUpdate()
+    {
+        while (AssetBundleQueue.Count > 0)
         {
             var ab = AssetBundleQueue.Dequeue();
             ab.Unload(false);
         }
     }
+    */
 
     private byte[] MyLoader(ref string filepath)
     {
@@ -48,16 +76,23 @@ public class MyFishLua01 : MonoBehaviour
     {
         luaEnv.DoString("HotFix(false)");
         luaEnv.Dispose();
+
+        while (AssetBundleQueue.Count > 0)
+        {
+            var ab = AssetBundleQueue.Dequeue();
+            ab.Unload(false);
+        }
     }
 
-    [LuaCallCSharp]
+    // 本地的
+    /* [LuaCallCSharp]
     public static GameObject LoadResource(string resName, string filePath)
     {
         GameObject go = GetGameObject(resName);
         if (!go)
         {
             string path = string.Format("{0}/{1}"
-                , Application.dataPath.Remove(Application.dataPath.Length-7,7), filePath);
+                , Application.dataPath.Remove(Application.dataPath.Length - 7, 7), filePath);
             AssetBundle ab = AssetBundle.LoadFromFile(path);
             go = ab.LoadAsset<GameObject>(resName);
             gameObjectDic.Add(resName, go);
@@ -72,6 +107,55 @@ public class MyFishLua01 : MonoBehaviour
         GameObject go = null;
         gameObjectDic.TryGetValue(resName, out go);
         return go;
+    }*/
+
+    public void LoadLua()
+    {
+        StartCoroutine(LoadLuaCorotine());
     }
 
+    private IEnumerator LoadLuaCorotine()
+    {
+        UnityWebRequest uwr = UnityWebRequest.Get(webPath + "MyFishLua01.lua.txt");
+        yield return uwr.SendWebRequest();
+        //luaEnv.AddLoader((ref string filepath) =>
+        //{
+        //    byte[] bytes = System.Text.Encoding.UTF8.GetBytes(uwr.downloadHandler.text);
+        //    return bytes;
+        //});
+        luaEnv.DoString(uwr.downloadHandler.text);
+        //luaEnv.DoString("require 'MyFishLua01';");
+        luaEnv.DoString("HotFix(true)");
+    }
+
+    //网络的
+    [LuaCallCSharp]
+    public GameObject LoadResource(string resName, string filePath)
+    {
+        GameObject go = GetGameObject(resName);
+        if (!go)
+        {
+            StartCoroutine(LoadAB(resName, filePath));
+        }
+        return go;
+    }
+
+    [LuaCallCSharp]
+    public GameObject GetGameObject(string resName)
+    {
+        GameObject go = null;
+        gameObjectDic.TryGetValue(resName, out go);
+        return go;
+    }
+
+
+    private IEnumerator LoadAB(string resName, string filePath)
+    {
+        UnityWebRequest uwr = UnityWebRequest.GetAssetBundle(webPath + filePath);
+        yield return uwr.SendWebRequest();
+        AssetBundle ab = (uwr.downloadHandler as DownloadHandlerAssetBundle).assetBundle;
+        var go = ab.LoadAsset<GameObject>(resName);
+        gameObjectDic.Add(resName, go);
+        AssetBundleQueue.Enqueue(ab);
+    }
 }
